69025f5516dc500d982cb04ec3e5cab6c74c367a
mwhite13 <mwhite13@ggc.edu>
2019-04-22 13:39:04 -0400

    Object Detection and Hologram.io

--- parkingAssistant.py
+++ parkingAssistant.py
@@ -13,6 +13,14 @@ ECHO = 24
 
 occupied = False
 
+#close
+def close(signal, frame):
+	print("\nTurning off ultrasonic distance detection...\n")
+	GPIO.cleanup()
+	sys.exit(0)
+
+signal.signal(signal.SIGINT, close)
+
 #GPIO direction
 def sensor_setup():
 	GPIO.setup(TRIG, GPIO.OUT)
@@ -23,7 +31,9 @@ def sensor_setup():
 def distance():
 
 	GPIO.output(TRIG, True)
+
 	time.sleep(0.00001)
+
 	GPIO.output(TRIG, False)
 
 	startTime = time.time()
@@ -34,32 +44,50 @@ def distance():
 
 	while 1 == GPIO.input(ECHO):
 		stopTime = time.time()
+
 	TimeElapsed = stopTime - startTime
 
 
 	distance = (TimeElapsed * 34300) / 2
 
-return distance
+	return distance
 
 
-if __name__ == '__main__':
-	sensor_setup()
-	init_check()
-	while True:
-		if (occupied and (distance()  >= 7)) or (not  occupied  and (distance() < 7)):
-		time.
-def init_check():
-	occupied = True if distance() < 7 else False
-	print(occupied)
+#Detect Object
+def detection():
+	if(distance() <= 7):
+		print "detected"
+		print distance()
+	elif(distance() > 7):
+		print "No detection"
+		print distance()
 
-def  convertJSON(occupied):
+	time.sleep(5)
+
+
+#Convert to JSON
+def convertJSON(occupied):
 	dict_object = {
 	"occupied": occupied
 	}
 	return json.dumps(dict_object)
 
-def close(signal, frame):
-	print("\nTurning off ultrasonic distance detection...\n")
-	GPIO.cleanup()
-	sys.exit(0)
-signal.signal(signal.SIGINT, close)
+#Initialize
+def initialize():
+	occupied = True if distance() < 7 else False
+	convertJSON(occupied)
+	print (occupied)
+
+
+
+if __name__ == '__main__':
+
+	sensor_setup()
+	initialize()
+
+	while True:
+		detection()
+
+
+
+
--- test_sensor.py
+++ test_sensor.py
@@ -1,76 +1,49 @@
-#Libraries
 import RPi.GPIO as GPIO
 import time
 import signal
 import sys
-import subprocess
-import json
- 
-#GPIO Mode (BOARD / BCM)
+
+# use Raspberry Pi board pin numbers
 GPIO.setmode(GPIO.BCM)
- 
-#set GPIO Pins
-GPIO_TRIGGER = 18
-GPIO_ECHO = 24
 
-occupied = False
- 
-#set GPIO direction (IN / OUT)
+# set GPIO Pins
+pinTrigger = 18
+pinEcho = 24
 
-def setup():
-    GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-    GPIO.setup(GPIO_ECHO, GPIO.IN)
- 
-def init_check():
-    occupied = True if distance() >= 7 else False
-    print "Occupied" 
+def close(signal, frame):
+	print("\nTurning off ultrasonic distance detection...\n")
+	GPIO.cleanup() 
+	sys.exit(0)
 
-#set distance
-def distance():
-    #set trigger to LOW
-    GPIO.output(GPIO_TRIGGER, False)
-    print "Waiting......."
-    time.sleep(1)
-    # set Trigger to HIGH
-    print "Calculating ......"
-    GPIO.output(GPIO_TRIGGER, True)
- 
-    # set Trigger after 0.01ms to LOW
-    time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER, False)
- 
-    StartTime = time.time()
-    StopTime = time.time()
- 
-    # save StartTime
-    while GPIO.input(GPIO_ECHO) == 0:
-        StartTime = time.time()
- 
-    # save time of arrival
-    while GPIO.input(GPIO_ECHO) == 1:
-        StopTime = time.time()
- 
-    # time difference between start and arrival
-    TimeElapsed = StopTime - StartTime
-    # multiply with the sonic speed (34300 cm/s)
-    # and divide by 2, because there and back
-    distance = (TimeElapsed  *17150)
-    distance = round(distance, 2)
- 
-    return distance
- 
-if __name__ == '__main__':
+signal.signal(signal.SIGINT, close)
 
-    setup()
-    init_check()
-    try:
-        while True:
-          if(occupied and (distance()>= 7))or(not occupied and (distance() <7)):
-            dist = distance()
-            print ("Measured Distance = %.1f cm" % dist)
-            time.sleep(5)
- 
-        # Reset by pressing CTRL + C
-    except KeyboardInterrupt:
-        print("Measurement stopped by User")
-        GPIO.cleanup()
+# set GPIO input and output channels
+GPIO.setup(pinTrigger, GPIO.OUT)
+GPIO.setup(pinEcho, GPIO.IN)
+
+while True:
+	# set Trigger to HIGH
+	GPIO.output(pinTrigger, True)
+	# set Trigger after 0.01ms to LOW
+	time.sleep(0.00001)
+	GPIO.output(pinTrigger, False)
+
+	startTime = time.time()
+	stopTime = time.time()
+
+	# save start time
+	while 0 == GPIO.input(pinEcho):
+		startTime = time.time()
+
+	# save time of arrival
+	while 1 == GPIO.input(pinEcho):
+		stopTime = time.time()
+
+	# time difference between start and arrival
+	TimeElapsed = stopTime - startTime
+	# multiply with the sonic speed (34300 cm/s)
+	# and divide by 2, because there and back
+	distance = (TimeElapsed * 34300) / 2
+
+	print ("Distance: %.1f cm" % distance)
+	time.sleep(1)
